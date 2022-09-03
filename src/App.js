8b80d7e0bd9e0a26955ede42129ea1cf6ff49d97
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-09-03 23:16:38 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Transition, CSSTransition } from "react-transition-group";
+import { useState } from "react";
+
+const duration = 1000;
+
+const defaultStyle = {
+  transition: `opacity${duration}ms ease-in-out`,
+  opacity: 0,
+};
+
+const transitionStyles = {
+  entering: {
+    opacity: 1,
+    backgroundColor: "purple",
+    height: "80px",
+  },
+  entered: {
+    opacity: 1,
+    backgroundColor: "red",
+    height: "80px",
+  },
+  exiting: {
+    opacity: 1,
+    backgroundColor: "yellow",
+    height: "80px",
+  },
+  exited: {
+    opacity: 1,
+    backgroundColor: "green",
+    height: "80px",
+  }
+}
+
+const FadeComponent = ({ in: inProps }) => {
+  return (
+    <Transition
+      
+      in={inProps}
+
+      onEnter={(node) => {
+        console.log("enter node", node)
+      }}  
+
+      onEntering={(node) => {
+        console.log("entering node",node)
+      }} 
+
+      onExit={(node) => {
+        console.log("exit node", node)
+      }}  
+
+      onExiting={(node) => {
+        console.log("exiting node", node)
+      }}  
+    
+      timeout={duration}>
+      
+      {state => (
+         <div style={{
+          ...defaultStyle,
+           ...transitionStyles[state],
+        }}>
+          {JSON.stringify(state)}
+                </div>
+     )}
+    </Transition>
+  );
+};
 
 function App() {
+  const [show, setShow] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+      React Animations <button onClick={() => { setShow(!show) }}>Başlat</button>
+      
+      <FadeComponent in={show} />
+
+      <CSSTransition in={show} timeout={500} classNames="alert" > 
+        
+     </CSSTransition>
+
+
+  </div>
+  )
 }
 
 export default App;
